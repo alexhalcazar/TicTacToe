ad9c0e4a60afaffefccd9136ee837b219bccb3c5
alexhalcazar <alexhalcazar369@gmail.com>
2023-04-10 10:42:30 -0700

    Testing changes

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -9,7 +9,7 @@ bool p1Turn = true;
 int main(){
     bool gameOver = false;
 
-    char squares[9] = {1,2,3,4,5,6,7,8,9};
+    char squares[9] = {'1','2','3','4','5','6','7','8','9'};
     
     while(!gameOver) {
         drawBoard(squares);
